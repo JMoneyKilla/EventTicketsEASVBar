f65b3c7db3957cc440c9694a8f4cb3a4d327ecd9
Madsp15 <madsp@hotmail.dk>
2023-03-21 12:45:52 +0100

    added some CRUD passthroughs to the manaager from the new Facade

--- src/dk/javahandson/bll/TicketManager.java
+++ src/dk/javahandson/bll/TicketManager.java
@@ -1,4 +1,32 @@
 package dk.javahandson.bll;
 
+import dk.javahandson.be.Ticket;
+import dk.javahandson.dal.DataAccessFacade;
+
+import java.sql.SQLException;
+
 public class TicketManager {
+    DataAccessFacade dataAccessFacade = new DataAccessFacade();
+
+    public void createTicket(Ticket ticket) {
+        try {
+            dataAccessFacade.createTicket(ticket);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void updateTicket(Ticket ticket) {
+        try {
+            dataAccessFacade.updateTicket(ticket);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void deleteTicket(Ticket ticket) {
+        try {
+            dataAccessFacade.deleteTicket(ticket);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/dk/javahandson/bll/UserManager.java
+++ src/dk/javahandson/bll/UserManager.java
@@ -1,4 +1,33 @@
 package dk.javahandson.bll;
 
+
+import dk.javahandson.be.User;
+import dk.javahandson.dal.DataAccessFacade;
+
+import java.sql.SQLException;
+
 public class UserManager {
+    DataAccessFacade dataAccessFacade = new DataAccessFacade();
+
+    public void createUser(User user) {
+        try {
+            dataAccessFacade.createUser(user);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void updateUser(User user) {
+        try {
+            dataAccessFacade.updateUser(user);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void deleteUser(User user) {
+        try {
+            dataAccessFacade.deleteUser(user);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
