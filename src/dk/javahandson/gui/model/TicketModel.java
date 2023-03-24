6ccae652fe2cabd5411d64b13b63190e8febe43c
JMoneyKilla <112935968+JMoneyKilla@users.noreply.github.com>
2023-03-24 12:34:39 +0100

    merge conflicts

--- src/dk/javahandson/gui/model/TicketModel.java
+++ src/dk/javahandson/gui/model/TicketModel.java
@@ -16,12 +16,13 @@ public class TicketModel {
     private final ObservableList<Ticket> tickets;
     private ManagerFacade bll = new ManagerFacade();
 
-    public static TicketModel getInstance(){
-        if(instance == null)
+    public static TicketModel getInstance() {
+        if (instance == null)
             instance = new TicketModel();
         return instance;
     }
-    public ObservableList<Ticket> getTickets(){
+
+    public ObservableList<Ticket> getTickets() {
         return tickets;
     }
 
@@ -30,39 +31,41 @@ public class TicketModel {
     }
 
 
-    public void fetchAllTickets()
-    {
+    public void fetchAllTickets() {
         tickets.clear();
 
     }
-    public void updateTicketsToEvent(int id){
+
+    public void updateTicketsToEvent(int id) {
         List<Ticket> eventTickets = bll.getTicketsByEventId(id);
         tickets.clear();
         tickets.addAll(eventTickets);
     }
-    public void createTicket(Ticket ticket)
-    {
+
+    public void createTicket(Ticket ticket) {
         try {
             bll.createTicket(ticket);
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-<<<<<<< HEAD
-    public void batchCreateTickets(int records, int eventId, String type)
-    {
+
+    public void batchCreateTickets(int records, int eventId, String type) {
         try {
             bll.batchCreateTickets(records, eventId, type);
         } catch (Exception e) {
-=======
+            throw new RuntimeException(e);
+        }
+    }
 
-    public void updateCustomerToTicket(Ticket ticket, String customerName, String customerEmail){
+
+    public void updateCustomerToTicket(Ticket ticket, String customerName, String customerEmail) {
         Ticket updatedTicket = new Ticket(ticket.getUuid(), ticket.getEventId(), ticket.getType(), customerName, customerEmail);
         try {
             bll.updateTicket(updatedTicket);
         } catch (SQLException e) {
->>>>>>> development
             throw new RuntimeException(e);
         }
     }
 }
+
