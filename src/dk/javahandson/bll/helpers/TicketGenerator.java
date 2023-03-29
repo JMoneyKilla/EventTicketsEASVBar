fc02022196283acf2baf6efd5a69cfc2c822d937
JMoneyKilla <112935968+JMoneyKilla@users.noreply.github.com>
2023-03-30 01:12:21 +0200

    working ticket generator, complete but maybe needs tweaking

--- src/dk/javahandson/bll/helpers/TicketGenerator.java
+++ src/dk/javahandson/bll/helpers/TicketGenerator.java
@@ -32,8 +32,9 @@ public class TicketGenerator{
     }
 
     public void createCompleteVoucher(Voucher voucher, Event event) throws SQLException {
+        Random rd = new Random();
         String uuid = voucher.getUuid();
-        String voucherName = voucher.getType() + "_" + event.getName();
+        String voucherName = voucher.getType() + "_" + event.getName() + "_" + rd.nextInt(10000);
         int eventId = voucher.getEventId();
         createImageToSend(createQRCode(uuid, voucherName, eventId), event, voucherName);
     }
@@ -41,9 +42,7 @@ public class TicketGenerator{
     public File createQRCode(String uuid, String custEvent, int eventID) {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         BitMatrix bitMatrix = null;
-        Random rd = new Random();
-        int rdInt = rd.nextInt(10000);
-        
+
         try {
             bitMatrix = qrCodeWriter.encode(uuid, BarcodeFormat.QR_CODE, 300, 300);
         } catch (WriterException e){
@@ -54,9 +53,8 @@ public class TicketGenerator{
         File dirCheck = new File(dir);
         if(!dirCheck.exists()) dirCheck.mkdirs();
 
-        String fileName = custEvent + rdInt + "_" + eventID + "QRCode.png";
+        String fileName = custEvent + "_" + eventID + "QRCode.png";
         File qrFile = new File(dir + fileName);
-        System.out.println(qrFile.getAbsolutePath());
 
         try{
             ImageIO.write(MatrixToImageWriter.toBufferedImage(bitMatrix), "png", qrFile.getAbsoluteFile());
@@ -68,7 +66,7 @@ public class TicketGenerator{
 
     public static void main(String[] args) throws SQLException {
         Ticket ticket = new Ticket("550e8400-e29b-41d4-a716-446655440000", 19, "VIP", "Julian", "julian@mail.dk");
-        Event event = new Event(19, "Birthday", "15:30", "19:00", "Gl Vardevej 78F", "crazy", 0, 0, 0, 0,
+        Event event = new Event(19, "Gigantic Balloon Party", "15:30", "19:00", "Gl Vardevej 78F", "crazy", 0, 0, 0, 0,
                 "29-03-2023", "29-03-2023");
         TicketGenerator ticketGenerator = new TicketGenerator();
         ticketGenerator.createCompleteTicket(ticket, event);
@@ -83,29 +81,42 @@ public class TicketGenerator{
         String timeEnd = event.getEndTime();
         int eventID = event.getId();
         BufferedImage qrImage = null;
+        BufferedImage easvLogo = null;
         try {
             qrImage = ImageIO.read(qrFile);
+            easvLogo = ImageIO.read(new File("resources/Pictures/easvLogo.png"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        BufferedImage ticketImage = new BufferedImage(300, 600, BufferedImage.TYPE_INT_RGB);
+        int x = 530;
+        int y = 740;
+        BufferedImage ticketImage = new BufferedImage(x, y, BufferedImage.TYPE_INT_RGB);
 
+        //places all graphics for image of ticket
         Graphics2D g2d = ticketImage.createGraphics();
         g2d.setColor(Color.WHITE);
-        g2d.fillRect(0,0,300,600);
-
+        g2d.fillRect(0,0,x,y);
         g2d.setColor(Color.BLACK);
-        g2d.setFont(new Font("Arial", Font.BOLD, 24));
-        g2d.drawString(eventName, 50, 50);
-        g2d.drawString(customerName, 50, 100);
-        g2d.drawString("START: " + dateStart, 50, 130);
-        g2d.drawString("     At: " + timeStart, 50, 160);
-        g2d.drawString("END: " + dateEnd, 50, 190);
-        g2d.drawString("     At: " + timeEnd, 50, 220);
+        g2d.setFont(new Font("Arial", Font.BOLD, 28));
+        g2d.drawString(eventName, (x/2)-(eventName.length()*7), 165);
+        g2d.setFont(new Font("Arial", Font.CENTER_BASELINE, 24));
+        g2d.drawString("START: " + dateStart, 150, 244);
+        g2d.drawString("     At: " + timeStart, 168, 284);
+        g2d.drawString("END: " + dateEnd, 165, 324);
+        g2d.drawString("     At: " + timeEnd, 168, 364);
+        g2d.drawString("Contact us:", 50, y- qrImage.getHeight());
+        g2d.setFont(new Font("Arial", Font.CENTER_BASELINE, 16));
+        g2d.drawString("Email:", 50, (y-qrImage.getHeight())+35);
+        g2d.drawString("Phone:", 50, (y-qrImage.getHeight())+105);
+        g2d.setFont(new Font("Arial", Font.CENTER_BASELINE, 12));
+        g2d.drawString("+45 65 69 42 01", 50, (y-qrImage.getHeight())+140);
+        g2d.drawString("ouremail@mailsource.dk", 50, (y-qrImage.getHeight())+70);
+        g2d.setFont(new Font("Arial", Font.ITALIC, 10));
+        g2d.drawString("This ticket belongs to...", 50, y-80);
+        g2d.drawString(customerName, 50, y-50);
 
-
-
-        g2d.drawImage(qrImage, 300-qrImage.getWidth(), 600-qrImage.getHeight(), null);
+        g2d.drawImage(easvLogo, 0, 0, null);
+        g2d.drawImage(qrImage, x-qrImage.getWidth(), y-qrImage.getHeight(), null);
 
 
         String dir = getClass().getResource("/").getFile() + "Pictures/Tickets/";
@@ -113,9 +124,9 @@ public class TicketGenerator{
         if(!dirCheck.exists()) dirCheck.mkdirs();
 
         String fileName = customerName + "_" + eventName + "_" + eventID + "Ticket.png";
-        File qrFile1 = new File(dir + fileName);
+        File ticketFile = new File(dir + fileName);
         try {
-            ImageIO.write(ticketImage, "png", qrFile1.getAbsoluteFile());
+            ImageIO.write(ticketImage, "png", ticketFile.getAbsoluteFile());
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
