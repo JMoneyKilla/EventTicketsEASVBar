1386bbdc60cb4a2dc712f994c788ca007077dfea
Madsp15 <madsp@hotmail.dk>
2023-04-13 13:02:59 +0200

    updated email helper

--- src/dk/javahandson/bll/helpers/EmailHandler.java
+++ src/dk/javahandson/bll/helpers/EmailHandler.java
@@ -1,13 +1,15 @@
-package dk.javahandson.bll;
+package dk.javahandson.bll.helpers;
 
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 
-public class emailHandler {
+public class EmailHandler {
 
-    public void openEmail(String recipiente, String subject, String body){
+    public void openEmail(String recipiente, String event){
         try {
+            String body = "Thank you for buying tickets for " + event + ". We hope you enjoy the event!";
+            String subject = "Event tickets for" + event;
             Desktop desktop = Desktop.getDesktop();
             URI uri = URI.create("mailto:" + recipiente + "?subject=" + subject + "&body=" + body);
             desktop.mail(uri);
